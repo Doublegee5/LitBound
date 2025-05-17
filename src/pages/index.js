93b07571416bda94d2b75e9d06e9ab0f72a52dc8
Doublegee5 <167867797+Doublegee5@users.noreply.github.com>
2025-05-17 15:06:30 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
-import Navbar from '../../lib/Navbar';
+import Navbar from '../lib/Navbar';
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen bg-white">
       <Navbar />
-      <div className="flex items-center justify-center h-[80vh]">
-        <h1 className="text-4xl font-bold">Welcome to LitBound</h1>
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold text-center">Welcome to LitBound!</h1>
       </div>
     </main>
   );
