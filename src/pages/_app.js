68dc0bab19aed569a161d5bb7b4f76ea6a492baf
Doublegee5 <167867797+Doublegee5@users.noreply.github.com>
2025-05-19 16:08:09 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,27 +1,15 @@
 import '../styles/globals.css';
-import { WagmiConfig, createConfig, configureChains } from 'wagmi';
-import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
-import { publicProvider } from 'wagmi/providers/public';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { LensProvider } from '@lens-protocol/react-web';
+import { ConnectKitProvider } from 'connectkit';
 
-const { chains, publicClient } = configureChains(
-  [mainnet, polygon, optimism, arbitrum],
-  [publicProvider()]
-);
-
-const config = createConfig({
-  autoConnect: true,
-  publicClient,
-});
-
-const queryClient = new QueryClient();
-
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
-    <WagmiConfig config={config}>
-      <QueryClientProvider client={queryClient}>
+    <LensProvider apiUrl="https://api.lens.dev">
+      <ConnectKitProvider>
         <Component {...pageProps} />
-      </QueryClientProvider>
-    </WagmiConfig>
+      </ConnectKitProvider>
+    </LensProvider>
   );
 }
+
+export default MyApp;
