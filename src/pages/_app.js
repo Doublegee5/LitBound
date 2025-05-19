d82e04d3ec08a922629b2a579ed416d39a0773d5
Doublegee5 <167867797+Doublegee5@users.noreply.github.com>
2025-05-19 15:00:24 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 import { WagmiConfig, createConfig, configureChains } from 'wagmi';
 import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
-import { publicProvider } from 'wagmi/providers/public';
+import { publicProvider } from '@wagmi/core/providers/public';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const { chains, publicClient } = configureChains(
