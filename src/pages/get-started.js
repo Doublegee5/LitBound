2f8b36168f9745430694a05f871b9de8217f6094
Doublegee5 <167867797+Doublegee5@users.noreply.github.com>
2025-05-19 01:18:53 +0100

    Create get-started.js

---
+++ src/pages/get-started.js
@@ -0,0 +1,7 @@
+export default function GetStarted() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white text-center p-8">
+      <h1 className="text-3xl font-bold">Let's Get Started!</h1>
+    </div>
+  );
+}
